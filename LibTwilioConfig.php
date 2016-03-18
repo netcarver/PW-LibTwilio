96e8568d8c81a9089f4fa53713f163662aa5bd7b
Netcarver <nc.github@netcarving.com>
2016-03-18 17:15:05 +0000

    Fix mixed indentation.

--- LibTwilioConfig.php
+++ LibTwilioConfig.php
@@ -1,36 +1,36 @@
 <?php
 
 class LibTwilioConfig extends ModuleConfig {
-	public function getDefaults() {
-		return array(
-			"id" => "",
-			"token" => "",
-			"from_number" => ""
-		);
-	}
+    public function getDefaults() {
+        return array(
+            "id" => "",
+            "token" => "",
+            "from_number" => ""
+        );
+    }
 
-	public function getInputfields() {
-		$inputfields = parent::getInputfields();
+    public function getInputfields() {
+        $inputfields = parent::getInputfields();
 
-		$f = $this->modules->get('InputfieldText');
-		$f->attr('name', 'id');
-		$f->label = $this->_('Twilio SID');
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'id');
+        $f->label = $this->_('Twilio SID');
         $f->notes = $this->_('From your Twilio Account (at https://www.twilio.com/user/account)');
         $f->columnWidth = 50;
-		$inputfields->add($f);
+        $inputfields->add($f);
 
-		$f = $this->modules->get('InputfieldText');
-		$f->attr('name', 'token');
-		$f->label = $this->_('Twilio API Token');
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'token');
+        $f->label = $this->_('Twilio API Token');
         $f->columnWidth = 50;
-		$inputfields->add($f);
+        $inputfields->add($f);
 
-		$f = $this->modules->get('InputfieldText');
-		$f->attr('name', 'from_number');
-		$f->label = $this->_('Your Twilio Number');
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'from_number');
+        $f->label = $this->_('Your Twilio Number');
         $f->description = $this->_('Will be used as the "from" number when sending messages or starting calls.');
-		$inputfields->add($f);
+        $inputfields->add($f);
 
-		return $inputfields;
-	}
+        return $inputfields;
+    }
 }
